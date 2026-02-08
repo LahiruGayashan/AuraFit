161e054f2121a7a13a80c5040c3be907a00645ce
Lahiru Gayashan <72918623+LahiruGayashan@users.noreply.github.com>
2026-02-08 16:07:59 +0530

    Add bottom navigation and placeholder pages
    Convert MyApp to a StatefulWidget and implement a BottomNavigationBar to switch between four placeholder pages (Home, Add, Favorite, Profile). Add new page files for HomePage, AddPage, FavoritePage, and ProfilePage with simple SafeArea scaffolds; remove the unused calculator page. Also set app title to "AuraFit" and hide the debug banner.

--- aurafit_app/lib/main.dart
+++ aurafit_app/lib/main.dart
@@ -1,44 +1,68 @@
+import 'package:aurafit_app/pages/add_page.dart';
+import 'package:aurafit_app/pages/favorite_page.dart';
+import 'package:aurafit_app/pages/home_page.dart';
+import 'package:aurafit_app/pages/profile_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
+  final List<Widget> _pages = [
+    HomePage(),
+    AddPage(),
+    FavoritePage(),
+    ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 12;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
+      title: "AuraFit",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blueAccent,
+          unselectedItemColor: CupertinoColors.inactiveGray,
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            }
+            );
+            
+          },
+          items: [
+            BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home"
+            ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: "Add"
+            ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favorite"
+            ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: "Profile"
+            ),
+            ],
+        ),
+        body: _pages[currentIndex],
+      ),
     );
   }
 }
